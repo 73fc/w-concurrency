effc65671ac3ecc7825d2f2ed353e7e46f4f294f
73fc <542591851@qq.com>
2024-05-21 11:01:41 -0700

    feature: support concurrent metrics

---
+++ examples/metrics.rs
@@ -0,0 +1,43 @@
+use anyhow::Result;
+use rand::Rng;
+use std::{thread, time::Duration};
+use wconcurrency::Metrics;
+
+const N: usize = 2;
+const M: usize = 4;
+fn main() -> Result<()> {
+    let metrics = Metrics::new();
+
+    println!("{:?}", metrics.snapshot());
+
+    for idx in 0..N {
+        task_worker(idx, metrics.clone());
+    }
+
+    for _ in 0..M {
+        request_woker(metrics.clone());
+    }
+
+    loop {
+        thread::sleep(Duration::from_secs(2));
+        println!("{:?}", metrics.snapshot());
+    }
+    //Ok(())
+}
+
+fn task_worker(idx: usize, metrics: Metrics) {
+    thread::spawn(move || loop {
+        let mut rng = rand::thread_rng();
+        thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
+        metrics.inc(format!("call.thread.woker.{}", idx)).unwrap();
+    });
+}
+
+fn request_woker(metrics: Metrics) {
+    thread::spawn(move || loop {
+        let mut rng = rand::thread_rng();
+        thread::sleep(Duration::from_millis(rng.gen_range(50..1000)));
+        let page = rng.gen_range(1..5);
+        metrics.inc(format!("req.page.{}", page)).unwrap();
+    });
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 mod matrix;
+mod metrics;
 mod vector;
 pub use matrix::*;
+pub use metrics::Metrics;
 pub use vector::*;
