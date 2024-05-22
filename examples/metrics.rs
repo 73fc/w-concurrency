8a4366cefabf248d5ee765a428993310eee4d2d6
73fc <542591851@qq.com>
2024-05-22 00:14:07 -0700

    fix: do not use unwrap

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -11,33 +11,44 @@ fn main() -> Result<()> {
     println!("{:?}", metrics.snapshot());
 
     for idx in 0..N {
-        task_worker(idx, metrics.clone());
+        task_worker(idx, metrics.clone())?;
     }
 
     for _ in 0..M {
-        request_woker(metrics.clone());
+        request_woker(metrics.clone())?;
     }
 
     loop {
         thread::sleep(Duration::from_secs(2));
         println!("{:?}", metrics.snapshot());
     }
-    //Ok(())
+    #[allow(unused)]
+    Ok(())
 }
 
-fn task_worker(idx: usize, metrics: Metrics) {
-    thread::spawn(move || loop {
-        let mut rng = rand::thread_rng();
-        thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
-        metrics.inc(format!("call.thread.woker.{}", idx)).unwrap();
+fn task_worker(idx: usize, metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::thread_rng();
+            thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
+            metrics.inc(format!("call.thread.woker.{}", idx))?;
+        }
+        #[allow(unused)]
+        Ok::<_, anyhow::Error>(())
     });
+    Ok(())
 }
 
-fn request_woker(metrics: Metrics) {
-    thread::spawn(move || loop {
-        let mut rng = rand::thread_rng();
-        thread::sleep(Duration::from_millis(rng.gen_range(50..1000)));
-        let page = rng.gen_range(1..5);
-        metrics.inc(format!("req.page.{}", page)).unwrap();
+fn request_woker(metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::thread_rng();
+            thread::sleep(Duration::from_millis(rng.gen_range(50..1000)));
+            let page = rng.gen_range(1..5);
+            metrics.inc(format!("req.page.{}", page))?;
+        }
+        #[allow(unused)]
+        Ok::<_, anyhow::Error>(())
     });
+    Ok(())
 }
